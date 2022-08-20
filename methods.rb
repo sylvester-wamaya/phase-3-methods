ca21715c4620e7697b5921737747cbc6fbced23c
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2022-08-20 10:52:33 +0300

    commit tree 5a1185cef5b1e5360e51bce32a45b0057060be3d

--- methods.rb
+++ methods.rb
@@ -1,1 +1,38 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+greet_programmer
+
+def greet name
+    puts "Hello, #{name}!"
+end
+
+greet "Naureen"
+
+def greet_with_default name="programmer"
+    puts "Hello, #{name}!"
+end
+
+greet_with_default
+greet_with_default "Sunny"
+
+def add num1, num2
+    num1 + num2
+end
+
+SUM = add 1, 2
+puts SUM
+
+
+def halve number
+    if number.class != Integer
+        return nil
+    end
+        number/2 
+    end
+
+    RESULT = halve 4
+    puts RESULT
+
